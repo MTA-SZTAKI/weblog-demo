0538b42a412a99213f5f007bf96e13c4876a1913
Molnár Péter <molnarp@ilab.sztaki.hu>
2013-07-01 18:21:01 +0200

    Removed sensitive password from config. Added lib directory to cleaned dirs. Removed executables. Cleaned up pom. Removed owner info from ddl scripts. Removed unused main class. Removed configuration files. Removed sample logs.

--- ddl_scripts/weblog_event_tables.sql
+++ ddl_scripts/weblog_event_tables.sql
@@ -2,32 +2,24 @@
 -- weblog_event_incoming
 --------------------------------------------------------
 -- Table: weblog_event_incoming
-
+--------------------------------------------------------
 -- DROP TABLE weblog_event_incoming;
-
 CREATE TABLE weblog_event_incoming
 (
   virtual_host character varying(1000) not null,
   request_timestamp timestamp(6) without time zone not null,
-
   event_group character varying(2000),
-  -- events: page view, displaying an email, making a contract etc.
   event character varying(2000),
-
-  -- visitor ids: a local id valid for the given host, and a global, host-independent id
   visitor_ip character varying(15),
   visitor_hostname character varying(2048),
   visitor_ip_resolv_time timestamp(6) without time zone,
   visitor_ip_resolv_expire timestamp(6) without time zone,
-
   http_request character varying(4000),
   inet_protocol character varying(128),
   http_method character varying(128),
   http_status_code integer,
   http_request_cookie character varying(4000),
   http_response_cookie character varying(4000),
-
-  -- full normalized url, then the parts
   request_url character varying(4000),
   request_url_parameter character varying(3000),
   request_url_extension character varying(128),
@@ -50,18 +42,12 @@ CREATE TABLE weblog_event_incoming
   agent_layout_resolution character varying(512),
   auth_user character varying(512),
   bytes_sent integer
-)
-WITH (
-  OIDS=FALSE
 );
-ALTER TABLE weblog_event_incoming
-  OWNER TO longneck;
 
 --------------------------------------------------------
 --  weblog_event_incoming_err
 --------------------------------------------------------
 -- DROP TABLE weblog_event_incoming_err;
-
 CREATE TABLE weblog_event_incoming_err
 (
   error_time timestamp without time zone,
@@ -74,20 +60,12 @@ CREATE TABLE weblog_event_incoming_err
   document_url character varying(2000),
   document_line character varying(100),
   document_column character varying(2000)
-)
-WITH (
-  OIDS=FALSE
 );
-ALTER TABLE weblog_event_incoming_err
-  OWNER TO longneck;
-
 
 --------------------------------------------------------
 -- Table: weblog_event_incoming_host
 --------------------------------------------------------
-
 -- DROP TABLE weblog_event_incoming_host;
-
 CREATE TABLE weblog_event_incoming_host
 (
   host_name character varying(4000),
@@ -108,18 +86,12 @@ CREATE TABLE weblog_event_incoming_host
   level13 character varying(4000),
   level14 character varying(4000),
   level15 character varying(4000)
-)
-WITH (
-  OIDS=FALSE
 );
-ALTER TABLE weblog_event_incoming_host
-  OWNER TO longneck;
 
 --------------------------------------------------------
 --  weblog_event_incoming_host_err
 --------------------------------------------------------
 -- DROP TABLE weblog_event_incoming_host_err;
-
 CREATE TABLE weblog_event_incoming_host_err
 (
   error_time timestamp without time zone,
@@ -132,9 +104,4 @@ CREATE TABLE weblog_event_incoming_host_err
   document_url character varying(2000),
   document_line character varying(100),
   document_column character varying(2000)
-)
-WITH (
-  OIDS=FALSE
 );
-ALTER TABLE weblog_event_incoming_host_err
-  OWNER TO longneck;
