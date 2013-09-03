04d9e3a5455a66622cf07673733a845788428e79
sidlo <csaba.sidlo@gmail.com>
2013-09-03 17:34:22 +0200

    scripts made more beautiful, added alternative create database command for possible locale problems

--- ddl_scripts/pgsql_create_database.sql
+++ ddl_scripts/pgsql_create_database.sql
@@ -3,3 +3,4 @@
 --
 create user weblog_demo password '123456';
 create database weblog_demo encoding 'utf8' owner weblog_demo;
+-- create database weblog_demo encoding ='utf8' LC_CTYPE = 'en_US.utf8' LC_COLLATE = 'en_US.utf8' owner = weblog_demo template = template0;
